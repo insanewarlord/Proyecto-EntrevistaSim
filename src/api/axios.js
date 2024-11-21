4b412c5c1025edfddf7b16fe394b250cf0083955
Juan David Ramirez <juan.grismaldo@correounivalle.edu.co>
2024-11-21 17:20:43 -0500

    version sin .env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,21 +1,14 @@
 import axios from "axios";
+// con condiciones de subida a producción
 
-const isProduction = import.meta.env.VITE_ENV === "production";
-console.log("isProduction", isProduction);
-console.log("import.meta.env", import.meta.env.VITE_APP_BASE_URL_PROD);
-console.log("import.meta.env", import.meta.env.VITE_APP_BASE_URL_DEV);
 const instance = axios.create({
-    baseURL: isProduction
-        ? (import.meta.env.VITE_APP_BASE_URL_PROD || "https://plataformainterviewsim-180808156072.us-central1.run.app/api")
-        : (import.meta.env.VITE_APP_BASE_URL_DEV || "http://localhost:4000/api"),
+    baseURL: "https://plataformainterviewsim-180808156072.us-central1.run.app/api", 
     withCredentials: true,
 });
 
-
+// Instancia para las rutas de entrevista
 const instanceInterview = axios.create({
-    baseURL: isProduction
-        ? (import.meta.env.VITE_APP_BASE_URL_PROD || "https://plataformainterviewsim-180808156072.us-central1.run.app/interview")
-        : (import.meta.env.VITE_APP_BASE_URL_DEV || "http://localhost:4000/interview"),
+    baseURL: "https://plataformainterviewsim-180808156072.us-central1.run.app/interview",
     withCredentials: true,
 });
 

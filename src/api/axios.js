f808295cdcbddf1405266ebc2d41fe13b31d8f93
Juan David Ramirez <juan.grismaldo@correounivalle.edu.co>
2024-11-20 20:52:33 -0500

    version now

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,30 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:`${import.meta.env.VITE_SERVER_CONNECTION}/api`,
+    baseURL: `${import.meta.env.VITE_SERVER_CONNECTION}/api`,
     withCredentials: true,
 });
 
 const instanceInterview = axios.create({
-    baseURL: `${import.meta.env.VITE_SERVER_CONNECTION}/interwiew`,
+    baseURL: `${import.meta.env.VITE_SERVER_CONNECTION}/interview`,
     withCredentials: true,
 });
+
+// Interceptor para manejar errores
+instance.interceptors.response.use(
+    response => response,
+    error => {
+        console.error("Error en la solicitud:", error.response ? error.response.data : error.message);
+        return Promise.reject(error);
+    }
+);
+
+instanceInterview.interceptors.response.use(
+    response => response,
+    error => {
+        console.error("Error en la solicitud:", error.response ? error.response.data : error.message);
+        return Promise.reject(error);
+    }
+);
 
 export { instance, instanceInterview };

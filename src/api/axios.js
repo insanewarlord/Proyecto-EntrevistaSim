617934d5248e81b23f615ea616a45fa3400e5808
Juan David Ramirez <juan.grismaldo@correounivalle.edu.co>
2024-11-21 16:23:42 -0500

    cors problem

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 const isProduction = import.meta.env.VITE_ENV === "production";
-
+console.log("isProduction", isProduction);
+console.log("import.meta.env", import.meta.env.VITE_APP_BASE_URL_PROD);
+console.log("import.meta.env", import.meta.env.VITE_APP_BASE_URL_DEV);
 const instance = axios.create({
     baseURL: isProduction
         ? import.meta.env.VITE_APP_BASE_URL_PROD
         : import.meta.env.VITE_APP_BASE_URL_DEV,
     withCredentials: true,
 });
+
 
 const instanceInterview = axios.create({
     baseURL: isProduction

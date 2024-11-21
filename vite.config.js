c5611ad63b763b7e45fea001dc70470967951ec4
Juan David Ramirez <juan.grismaldo@correounivalle.edu.co>
2024-11-21 08:41:55 -0500

    version control

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const PORT = 4000
+
 
 export default defineConfig({
   plugins: [react()
 
   ],
-  server: { port: PORT , host: true },
-  preview: {port: PORT, host: true}
+
 });
